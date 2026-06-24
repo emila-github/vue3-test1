2f4ab7a98912cdd7635c46a7bdd47b4bcf9f28d7
wangchangjian <wangchangjian_yyb@email.fjpicc.com>
2026-06-24 16:31:47 +0800

    添加mock开发脚本配置，支持不同模式下的 Vite 启动

--- vite.config.ts
+++ vite.config.ts
@@ -7,27 +7,34 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import { mockPlugin } from './src/mock'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    mockPlugin(), // ★ 无后端时使用 Mock，有后端时删除此行 + 启用下方 proxy
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  console.log('mode:', mode)
+  const isMock = mode === 'mock'
+
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+      ...(isMock ? [mockPlugin()] : []), // ★ 无后端时使用 Mock，有后端时启用下方 proxy
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  // ===== 有后端时启用：删掉上方 mockPlugin()，取消下面注释 =====
-  // server: {
-  //   proxy: {
-  //     // 正则 ^/api(/|$) 只匹配 /api 或 /api/xxx，不会误匹配 /api-demo
-  //     '^/api(/|$)': {
-  //       target: 'http://localhost:8080',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
+    // 有后端时启用
+    server: isMock
+      ? undefined
+      : {
+          proxy: {
+            // 正则 ^/api(/|$) 只匹配 /api 或 /api/xxx，不会误匹配 /api-demo
+            '^/api(/|$)': {
+              target: 'http://localhost:8080',
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+          },
+        },
+  }
 })
